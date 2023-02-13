5c83f0058c8b2469527aa7f77ad1458ac9e5fd77
harratnewfel <harratnewfel@gmail.com>
2023-02-13 19:30:36 +0800

    remove default password for xwiki_sql_user

--- tools/validate_tfvars.sh
+++ tools/validate_tfvars.sh
@@ -1,7 +1,8 @@
 PROJECT_ID=$1
 REGION=$2
-zone_code1=$3
-zone_code2=$4
+XWIKI_SQL_USER_PASSWORD=$3
+zone_code1=$4
+zone_code2=$5
 
 
 if [ -z $PROJECT_ID ]; then
@@ -9,6 +10,9 @@ if [ -z $PROJECT_ID ]; then
   exit 1
 elif [ -z $REGION ] || [ $REGION = "global" ]; then
   echo "Error: '_REGION' is required, and it cannot be set to 'global'"
+  exit 1
+elif [ -z $XWIKI_SQL_USER_PASSWORD ]; then
+  echo "Error: '_XWIKI_SQL_USER_PASSWORD' is required, and it cannot be set to 'global'"
   exit 1
 elif [ -z $zone_code1 ] || [ -z $zone_code2 ]; then
   echo "Error: 'zone_code' is required"
