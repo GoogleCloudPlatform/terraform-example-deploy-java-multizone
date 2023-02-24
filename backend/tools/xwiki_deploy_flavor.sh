056816b413b4bb68123f43ed5c6b0c5da8e7d6f4
Andrew Yang <andrew.yang@cienet.com>
2023-02-24 18:37:50 +0800

    fix xwiki image name, add file lock for data migration (#5)

--- backend/tools/xwiki_deploy_flavor.sh
+++ backend/tools/xwiki_deploy_flavor.sh
@@ -23,6 +23,60 @@ XWIKI_FLAVOR_DATA_DIR="/home"
 MYSQL_IMPORT_SQL_FILE="xwiki_bk_14.10.sql"
 NFS_SHARE_FILE="file_${VERSION}.tar.gz"
 XWIKI_DATA_DIR="/var/lib/xwiki/data"
+FLAVOR_LOG_PREFIX="Deploy_flavor:"
+LOCK_FILE="${XWIKI_DATA_DIR}/store/import.lock"
+IMPORTED="${XWIKI_DATA_DIR}/store/imported"
+
+Migrate_Data() {
+  (
+    flock -x -n 200 || exit 1
+    if [ -f ${IMPORTED} ]; then
+      echo "${FLAVOR_LOG_PREFIX} Data migration has completed. Function returned."
+    else
+      CONNECTION_RETRIES=20
+      retries=0
+      while ! mysql -u${DB_USER} --password="${DB_PASSWORD}" -h${DB_HOST} xwiki -e "exit" --connect-timeout=20
+      do
+        retries=$((retries+1))
+        if [ "$retries" -lt $CONNECTION_RETRIES ]; then
+          echo "MySQL server is not ready for login. Retrying in 3 sec..."
+          sleep 3
+        else
+          echo "ERROR: Couldn't login MySQL server ${DB_HOST}!"
+        fi
+      done
+
+      TABLE_COUNT=$(mysql -u${DB_USER} --password="${DB_PASSWORD}" -h${DB_HOST} xwiki -e "show tables;" | wc -l)
+      if [ ${TABLE_COUNT} -gt 0 ] ; then
+        echo "Xwiki Flavor ${VERSION} table data has already exists. Skipping table data import!"
+      else
+        echo "Xwiki Flavor ${VERSION} table data does not exist! Importing table data..."
+        sudo tar zxvf ${XWIKI_FLAVOR_DATA_DIR}/xwiki_mysql_db_bk_14.10.4.tar.gz -C ${XWIKI_FLAVOR_DATA_DIR}/
+        mysql -u${DB_USER} --password="${DB_PASSWORD}" -h${DB_HOST} xwiki < ${XWIKI_FLAVOR_DATA_DIR}/${MYSQL_IMPORT_SQL_FILE}
+        if [ $? -eq 0 ]; then
+          echo "Importing table data has been completed!"
+        else
+          echo "ERROR: Failed to import table data."
+        fi
+      fi
+
+      if sudo test -d ${XWIKI_DATA_DIR}/store/file/xwiki ; then
+        echo "The Xwiki Flavor ${VERSION} share file has already been mounted on NFS folder. Skipping file extraction!"
+      else
+        echo "The Xwiki Flavor ${VERSION} share file does not exist on NFS folder. Extracting file..."
+        sudo tar -zxvf ${XWIKI_FLAVOR_DATA_DIR}/file_${VERSION}.tar.gz -C ${XWIKI_DATA_DIR}/store
+        if [ $? -eq 0 ]; then
+          echo "XWiki Flavor share file extraction to NFS folder completed!"
+        else
+          echo "ERROR: Failed to extract XWiki Flavor data to NFS folder."
+        fi
+      fi
+      echo "${FLAVOR_LOG_PREFIX} Data migration has completed."
+      date > ${IMPORTED}
+      sleep 3
+    fi
+  ) 200>$LOCK_FILE
+}
 
 CONNECTION_RETRIES=20
 retries=0
@@ -38,59 +92,25 @@ do
   fi
 done
 
-LOCK_FILE="${XWIKI_DATA_DIR}/store/import.lock"
-IMPORTED_FILE="${XWIKI_DATA_DIR}/store/imported"
-
-if [ -f "$LOCK_FILE" ]; then
-  echo "$LOCK_FILE exists, do nothing."
-elif [ -f "$IMPORTED_FILE" ]; then
-  echo "$IMPORTED_FILE exists, do nothing."
-else
-  sudo sh -c "date > '$LOCK_FILE'"
-  echo "$LOCK_FILE created with current time."
-
-  CONNECTION_RETRIES=20
+Migrate_Data
+if [ $? -eq 1 ]; then
+  echo "${FLAVOR_LOG_PREFIX} Data migration is in progress."
+  RETRY_INTERVAL=60
   retries=0
-  while ! mysql -u${DB_USER} --password="${DB_PASSWORD}" -h${DB_HOST} xwiki -e "exit" --connect-timeout=20
-  do
+  while true; do
     retries=$((retries+1))
-    if [ "$retries" -lt $CONNECTION_RETRIES ]; then
-      echo "MySQL server is not ready for login. Retrying in 3 sec..."
-      sleep 3
+    if [ -f ${IMPORTED} ]; then
+      echo "${FLAVOR_LOG_PREFIX} Data migration has completed."
+      break
     else
-      echo "ERROR: Couldn't login MySQL server ${DB_HOST}!"
-      exit 1
+      echo "${FLAVOR_LOG_PREFIX} Still waiting for data migration..."
     fi
+    if [ "$retries" -ge $RETRY_INTERVAL ]; then
+      echo "${FLAVOR_LOG_PREFIX} Data migration timed out."
+      break
+    fi
+    sleep 1
   done
-
-  TABLE_COUNT=$(mysql -u${DB_USER} --password="${DB_PASSWORD}" -h${DB_HOST} xwiki -e "show tables;" | wc -l)
-  if [ ${TABLE_COUNT} -gt 0 ] ; then
-  	echo "Xwiki Flavor ${VERSION} table data has already exists. Skipping table data import!"
-  else
-    echo "Xwiki Flavor ${VERSION} table data does not exist! Importing table data..."
-    sudo tar zxvf ${XWIKI_FLAVOR_DATA_DIR}/xwiki_mysql_db_bk_14.10.4.tar.gz -C ${XWIKI_FLAVOR_DATA_DIR}/
-  	mysql -u${DB_USER} --password="${DB_PASSWORD}" -h${DB_HOST} xwiki < ${XWIKI_FLAVOR_DATA_DIR}/${MYSQL_IMPORT_SQL_FILE}
-  	if [ $? -eq 0 ]; then
-  		echo "Importing table data has been completed!"
-  	else
-  		echo "ERROR: Failed to import table data."
-  	fi
-  fi
-
-  if sudo test -d ${XWIKI_DATA_DIR}/store/file/xwiki ; then
-  	echo "The Xwiki Flavor ${VERSION} share file has already been mounted on NFS folder. Skipping file extraction!"
-  else
-  	echo "The Xwiki Flavor ${VERSION} share file does not exist on NFS folder. Extracting file..."
-  	sudo tar -zxvf ${XWIKI_FLAVOR_DATA_DIR}/file_${VERSION}.tar.gz -C ${XWIKI_DATA_DIR}/store
-  	if [ $? -eq 0 ]; then
-  		echo "XWiki Flavor share file extraction to NFS folder completed!"
-  	else
-  		echo "ERROR: Failed to extract XWiki Flavor data to NFS folder."
-  	fi
-  fi
-
-  sudo sh -c "date > '$IMPORTED_FILE'"
-  echo "$IMPORTED_FILE created with current time."
 fi
 
 echo "-----Starting tomcat9-----"
