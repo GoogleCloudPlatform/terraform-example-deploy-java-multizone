ac03b9b220b637e170daf058d84d55424ea3f7b4
Don McCasland <donmccasland@google.com>
2023-04-13 14:19:00 -0700

    Update main.tf

--- infra/modules/filestore/main.tf
+++ infra/modules/filestore/main.tf
@@ -23,7 +23,7 @@ resource "google_filestore_instance" "xwiki" {
     modes   = ["MODE_IPV4"]
   }
   file_shares {
-    capacity_gb = 512
+    capacity_gb = 1024
     name        = "xwiki_file_share"
   }
 }
